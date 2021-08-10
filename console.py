44d6db0a85f715cbc042a5e145f2f9b1bf2cbe59
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-10 14:59:33 +0100

    zookeeper added

--- console.py
+++ console.py
@@ -2,3 +2,10 @@ import pdb
 from models.staff import Staff
 import repositories.staff_repository as staff_repository
 
+staff_repository.delete_all()
+
+new_staff_member = Staff("Zookeepy McGhee", "01/01/2002", "Penguins", 3)
+
+staff_repository.add_staff_member(new_staff_member)
+
+pdb.set_trace()
--- repositories/staff_repository.py
+++ repositories/staff_repository.py
@@ -4,3 +4,11 @@ from models.staff import Staff
 def delete_all():
     sql = "DELETE FROM staff"
     run_sql(sql)
+
+def add_staff_member(staff):
+    sql = "INSERT INTO staff (name, start_date, department, performance_rating) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [staff.name, staff.start_date, staff.department, staff.performance_rating]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    staff.id = id
+    return staff
